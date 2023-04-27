dbc830b45be344955783b6e732abe5228e8c3f9b
sabujmahonto <sabujmahonto@gmail.com>
2023-04-27 22:43:30 +0600

    player data or api added

---
+++ data/players.js
@@ -0,0 +1,22 @@
+ export const players = [
+    {
+        "id":1,
+        "name":"mesi",
+        "country":"argentina"
+    },
+    {
+        "id":2,
+        "name":"neymer",
+        "country":"brazil"
+    },
+    {
+        "id":3,
+        "name":"rolando",
+        "country":"portugal"
+    },
+    {
+        "id":4,
+        "name":"json roy",
+        "country":"england"
+    }
+]
